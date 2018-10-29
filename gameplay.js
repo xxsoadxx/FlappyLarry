228824cdf71e69936f37e8debf0d5df6dbe9fa30
Pierre <piepellegrin@gmail.com>
2018-10-29 19:41:02 +0100

    Add gameplay.js, index.html, phaser.min.js, assets

---
+++ gameplay.js
@@ -0,0 +1,139 @@
+    var config = {
+        type: Phaser.AUTO,
+        width: 288,
+        height: 512,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    var game = new Phaser.Game(config);
+    var base;
+    /*var platforms;
+    var player;
+    var cursors;
+    var stars;
+    var score = 0;
+    var scoreText;
+    var bombs;*/
+
+    function preload ()
+    {
+        /*this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});*/
+        this.load.image('0', 'assets/sprites/0.png');
+        this.load.image('1', 'assets/sprites/1.png');
+        this.load.image('2', 'assets/sprites/2.png');
+        this.load.image('3', 'assets/sprites/3.png');
+        this.load.image('4', 'assets/sprites/4.png');
+        this.load.image('5', 'assets/sprites/5.png');
+        this.load.image('6', 'assets/sprites/6.png');
+        this.load.image('7', 'assets/sprites/7.png');
+        this.load.image('8', 'assets/sprites/8.png');
+        this.load.image('9', 'assets/sprites/9.png');
+        this.load.image('background-day', 'assets/sprites/background-day.png');
+        this.load.image('background-night', 'assets/sprites/background-night.png');
+        this.load.image('base', 'assets/sprites/base.png');
+    }
+
+    function create ()
+    {
+        this.add.image(0, 0, 'background-day').setOrigin(0, 0);
+        base = this.physics.add.staticGroup();
+
+        base.create(0, 500, 'base');
+        base.width = 288;
+        /*this.add.image(400, 300, 'sky');
+        platforms = this.physics.add.staticGroup();
+
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
+
+        player = this.physics.add.sprite(100, 450, 'dude');
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+        player.body.setGravityY(300);
+        this.physics.add.collider(player, platforms);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'dude', frame: 4}],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        cursors = this.input.keyboard.createCursorKeys();
+
+        stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
+
+        stars.children.iterate(function(child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        });
+
+        this.physics.add.collider(stars, platforms);
+        this.physics.add.overlap(player, stars, collectStar, null, this);
+
+        scoreText = this.add.text(16, 16, 'score: 0', {fontSize: '32px', fill: '#000'});
+
+        bombs = this.physics.add.group();
+        this.physics.add.collider(bombs, platforms);
+        this.physics.add.collider(player, bombs, hitBomb, null, this);*/
+    }
+
+    function update ()
+    {
+        /*if(cursors.left.isDown)
+        {
+            player.setVelocityX(-160);
+            player.anims.play('left', true);
+        }
+        else if (cursors.right.isDown)
+        {
+            player.setVelocityX(160);
+
+            player.anims.play('right', true);
+        }
+        else
+        {
+            player.setVelocityX(0);
+
+            player.anims.play('turn');
+        }
+
+        if (cursors.up.isDown && player.body.touching.down)
+        {
+            player.setVelocityY(-490);
+        }*/
+    }
