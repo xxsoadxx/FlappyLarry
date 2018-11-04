f631ea25f219088df995d0266c09bd722d9510f7
Pierre <piepellegrin@gmail.com>
2018-11-04 17:53:13 +0100

    add animations

--- gameplay.js
+++ gameplay.js
@@ -19,21 +19,12 @@
     var game = new Phaser.Game(config);
     var base;
     var bird;
-    /*var platforms;
-    var player;
     var cursors;
-    var stars;
-    var score = 0;
-    var scoreText;
-    var bombs;*/
+    var logo;
+    var gameStarted = false;
 
     function preload ()
     {
-        /*this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});*/
         this.load.image('0', 'assets/sprites/0.png');
         this.load.image('1', 'assets/sprites/1.png');
         this.load.image('2', 'assets/sprites/2.png');
@@ -50,100 +41,38 @@
         this.load.image('blue-bird-downflap', 'assets/sprites/bluebird-downflap.png');
         this.load.image('blue-bird-midflap', 'assets/sprites/bluebird-midflap.png');
         this.load.image('blue-bird-upflap', 'assets/sprites/bluebird-upflap.png');
+        this.load.image('message', 'assets/sprites/message.png');
+        this.load.spritesheet('bird', 'assets/sprites/birds.png', {frameWidth: 100, frameHeight: 100});
     }
 
     function create ()
     {
-        this.add.image(0, 0, 'background-day').setOrigin(0, 0);
-        /*base = this.physics.add.staticGroup();
+        let bg = this.add.sprite(0, 0, 'background-day');
+        bg.setOrigin(0, 0);
 
-        base.create(130, 500, 'base');*/
+
+        logo = this.add.image(288/2, 512/2, 'message');
+        this.anims.create({
+            key: 'fly',
+            frames: [
+                { key: 'blue-bird-downflap' },
+                { key: 'blue-bird-midflap' },
+                { key: 'blue-bird-upflap' },
+            ],
+            frameRate: 8,
+            repeat: -1
+        });
+        bird = this.add.sprite(100, 300, 'bird').play('fly');
+        //bird.setCollideWorldBounds(true);
         base = this.add.tileSprite(0, 500, 600, 100, 'base');
-
-        bird = this.physics.add.sprite(100, 100, 'blue-bird-downflap');
-        /*this.add.image(400, 300, 'sky');
-        platforms = this.physics.add.staticGroup();
-
-        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-        platforms.create(600, 400, 'ground');
-        platforms.create(50, 250, 'ground');
-        platforms.create(750, 220, 'ground');
-
-        player = this.physics.add.sprite(100, 450, 'dude');
-        player.setBounce(0.2);
-        player.setCollideWorldBounds(true);
-        player.body.setGravityY(300);
-        this.physics.add.collider(player, platforms);
-
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [{ key: 'dude', frame: 4}],
-            frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
-            frameRate: 10,
-            repeat: -1
-        });
-
         cursors = this.input.keyboard.createCursorKeys();
-
-        stars = this.physics.add.group({
-            key: 'star',
-            repeat: 11,
-            setXY: { x: 12, y: 0, stepX: 70 }
-        });
-
-        stars.children.iterate(function(child) {
-            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-        });
-
-        this.physics.add.collider(stars, platforms);
-        this.physics.add.overlap(player, stars, collectStar, null, this);
-
-        scoreText = this.add.text(16, 16, 'score: 0', {fontSize: '32px', fill: '#000'});
-
-        bombs = this.physics.add.group();
-        this.physics.add.collider(bombs, platforms);
-        this.physics.add.collider(player, bombs, hitBomb, null, this);*/
     }
 
     function update ()
     {
-        
-        /*if(this.bird.angle < 20){
-            this.bird.angle += 1;
-        }*/
-        /*if(cursors.left.isDown)
-        {
-            player.setVelocityX(-160);
-            player.anims.play('left', true);
-        }
-        else if (cursors.right.isDown)
-        {
-            player.setVelocityX(160);
+        base.tilePositionX += 2.5;
 
-            player.anims.play('right', true);
+        if(cursors.space.isDown && gameStarted == false){
+            logo.destroy();
         }
-        else
-        {
-            player.setVelocityX(0);
-
-            player.anims.play('turn');
-        }
-
-        if (cursors.up.isDown && player.body.touching.down)
-        {
-            player.setVelocityY(-490);
-        }*/
     }
